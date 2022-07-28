213a787815893aac260a70e62e013ce7382791ca
Sary Hammad <82519050+SaryH@users.noreply.github.com>
2022-07-28 15:24:18 +0300

    Time: 6 ms (52.76%), Space: 44.7 MB (50.76%) - LeetHub

---
+++ 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int mostWordsFound(String[] sentences) {
+        int max=0;
+        for(String s:sentences){
+            String[] temp=s.split(" ");
+            max=Math.max(max,temp.length);
+        }
+        return max;
+    }
+}
